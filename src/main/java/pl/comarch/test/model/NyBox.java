9e67eb86ceab66879e6bf28317cc07b3c5729428
Mirek <kom.kmaj@gmail.com>
2018-12-19 14:40:43 +0100

    box2

---
+++ src/main/java/pl/comarch/test/model/NyBox.java
@@ -0,0 +1,33 @@
+package pl.comarch.test.model;
+
+public class NyBox {
+
+	private String rodzaj;
+	private Integer dlugosc;
+	private Integer szerokosc;
+
+	public String getRodzaj() {
+		return rodzaj;
+	}
+
+	public void setRodzaj(String rodzaj) {
+		this.rodzaj = rodzaj;
+	}
+
+	public Integer getDlugosc() {
+		return dlugosc;
+	}
+
+	public void setDlugosc(Integer dlugosc) {
+		this.dlugosc = dlugosc;
+	}
+
+	public Integer getSzerokosc() {
+		return szerokosc;
+	}
+
+	public void setSzerokosc(Integer szerokosc) {
+		this.szerokosc = szerokosc;
+	}
+
+}
